cce0fa203e2e03d24508a01c6dda139e9a409262
Myung Hyun Lee <lmhlionu@likelion.org>
2020-10-07 16:18:18 +0900

    test

--- main/bar_read.py
+++ main/bar_read.py
@@ -1,5 +1,5 @@
 import cv2
-import pyzbar.pyzbar as pyzbar
+import pyzbar.pyzbar import decode
 from django.conf import settings
 
 def bar_read(path):
@@ -7,7 +7,7 @@ def bar_read(path):
 
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    decoded = pyzbar.decode(imgray)
+    decoded = decode(imgray)
 
     for d in decoded:
         cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0]+d.rect[2], d.rect[1]+d.rect[3]), (0,0,255), 2)
